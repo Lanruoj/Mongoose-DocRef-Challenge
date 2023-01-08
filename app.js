44a3586912135020151b5659146a7dd4e999159c
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-09 10:06:57 +1100

    Folder restructure

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
+
+async function dbConnect() {
+  let databaseURL = "mongodb://localhost:27017/BookClub";
+  await mongoose.connect(databaseURL);
+  console.log("Database connected");
+}
+
+dbConnect();
